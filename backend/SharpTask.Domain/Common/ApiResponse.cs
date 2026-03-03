828baa77440d327ae93f456c10019ac8a1af06e4
Ángel González Muñoz <119265574+xNeuNoRo@users.noreply.github.com>
2026-03-03 17:20:53 -0400

    Refactor ApiResponse to use ApiError class for error handling and make properties read-only

--- backend/SharpTask.Domain/Common/ApiResponse.cs
+++ backend/SharpTask.Domain/Common/ApiResponse.cs
@@ -3,25 +3,26 @@ namespace SharpTask.Domain.Common;
 public class ApiResponse<T>
 {
     // Ok indica si la respuesta fue exitosa o no
-    public bool Ok { get; set; }
+    public bool Ok { get; private set; }
 
     // Data contiene la informacion de la respuesta en caso de exito, o null si hubo un error
-    public T? Data { get; set; }
+    public T? Data { get; private set; }
 
     // Error contiene la informacion del error en caso de que Ok sea false, o null si la respuesta fue exitosa
-    public object? Error { get; set; }
+    public ApiError? Error { get; private set; }
 
     // Constructor para respuestas exitosas
     public ApiResponse(T data)
     {
         Ok = true;
         Data = data;
+        Error = null;
     }
 
     // Constructor para respuestas de error
     public ApiResponse(string code, string message)
     {
         Ok = false;
-        Error = new { code, message };
+        Error = new ApiError(code, message);
     }
 }
