8eabb49f8788477370535d94cbd9b31c0c4854eb
Ángel González Muñoz <119265574+xNeuNoRo@users.noreply.github.com>
2026-03-03 15:03:09 -0400

    Rename TaskName to Title in TaskItem and move TaskState enum to its own file

--- backend/SharpTask.Domain/Entities/Tasks/TaskItem.cs
+++ backend/SharpTask.Domain/Entities/Tasks/TaskItem.cs
@@ -5,7 +5,7 @@ namespace SharpTask.Domain.Entities.Tasks;
 
 public class TaskItem : BaseEntity
 {
-    public required string Name { get; set; }
+    public required string Title { get; set; }
     public string? Description { get; set; }
     public TaskState Status { get; set; }
     public List<TaskChange> Changes { get; set; } = new();
