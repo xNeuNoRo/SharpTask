260b76a0355bbf2862e060b97b4634cdd0d5ea45
Ángel González Muñoz <119265574+xNeuNoRo@users.noreply.github.com>
2026-03-03 14:40:35 -0400

    Refactor TaskItem to use List<TaskChange> for Changes property

--- backend/SharpTask.Domain/Entities/Tasks/TaskItem.cs
+++ backend/SharpTask.Domain/Entities/Tasks/TaskItem.cs
@@ -7,6 +7,5 @@ public class TaskItem : BaseEntity
     public required string Name { get; set; }
     public string? Description { get; set; }
     public TaskStatus Status { get; set; }
-    public TaskChange[]? Changes { get; set; }
-    
+    public List<TaskChange> Changes { get; set; } = new();
 }
