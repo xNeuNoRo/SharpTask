8103b33406790d15cd2f669dde2b38294c61b2d4
Ángel González Muñoz <119265574+xNeuNoRo@users.noreply.github.com>
2026-03-04 16:38:18 -0400

    Add JsonIgnore attribute to Notes property in TaskItem for serialization control

--- backend/SharpTask.Domain/Entities/Tasks/TaskItem.cs
+++ backend/SharpTask.Domain/Entities/Tasks/TaskItem.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using SharpTask.Domain.Entities.Base;
 using SharpTask.Domain.Enums;
 
@@ -9,5 +10,7 @@ public class TaskItem : BaseEntity
     public string? Description { get; set; }
     public TaskState Status { get; set; }
     public List<TaskChange> Changes { get; set; } = new();
+
+    [JsonIgnore]
     public List<NoteItem> Notes { get; set; } = new();
 }
